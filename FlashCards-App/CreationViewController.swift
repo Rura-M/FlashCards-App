08223a618f8a2267b638106cd317c1b1f31c668d
Ruramai Mutefura <ruramaimutefura@gmail.com>
2021-03-13 02:49:50 -0600

    Cancel Button Working

---
+++ FlashCards-App/CreationViewController.swift
@@ -0,0 +1,28 @@
+//
+//  CreationViewController.swift
+//  FlashCards-App
+//
+//  Created by Ruramaimunashe Mutefura on 3/13/21.
+//  Copyright © 2021 Ruramaimunashe Mutefura. All rights reserved.
+//
+
+import UIKit
+
+class CreationViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func didTapOnCancel(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    
+
+
+}
